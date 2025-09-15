9df1f08a08d78709dd415ff471be2358f08182a0
Nen Pachani <nenpanchani@gmail.com>
2025-09-15 18:30:53 +0530

    Enhance Array_List class with additional ArrayList operations and sorting functionality

--- Array_List.java
+++ Array_List.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Array_List {
     public static void main(String[] args) {
@@ -8,5 +9,29 @@ public class Array_List {
         list.add(1);
 
         System.out.println(list);
+
+        int element = list.get(0);
+        System.out.println(element);
+
+        list.add(2, 2);
+        System.out.println(list);
+
+        list.set(0, 5);
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+
+        int size = list.size();
+        System.out.println(size);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        Collections.sort(list);
+        System.out.println(list);
+
     }
 }
