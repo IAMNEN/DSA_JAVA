b3fc2f6f37b33c063e47466c7fae247407fa14ce
Nen Pachani <nenpanchani@gmail.com>
2025-09-15 18:22:18 +0530

    Add initial implementation of Array_List class with basic ArrayList usage

---
+++ Array_List.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Array_List {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList();
+
+        list.add(0);
+        list.add(1);
+
+        System.out.println(list);
+    }
+}
