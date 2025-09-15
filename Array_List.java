d1ebc18310742f0cf7549988ca7313e7f9c640ea
Nen Pachani <nenpanchani@gmail.com>
2025-09-15 18:33:44 +0530

    Refactor Array_List class to improve code readability by adding spacing and comments

--- Array_List.java
+++ Array_List.java
@@ -5,31 +5,40 @@ public class Array_List {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList();
 
+
+        //add element in array list
         list.add(0);
         list.add(1);
 
         System.out.println(list);
 
+        //fetch element using index number
         int element = list.get(0);
         System.out.println(element);
 
+        //add in given index number
         list.add(2, 2);
         System.out.println(list);
 
+        //replace value in given index number
         list.set(0, 5);
         System.out.println(list);
 
+        //remove element using index number
         list.remove(2);
         System.out.println(list);
 
+        //find size of Arraylist
         int size = list.size();
         System.out.println(size);
 
+        //fetch element using For loop
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
         }
         System.out.println();
 
+        //shorting array list 
         Collections.sort(list);
         System.out.println(list);
 
