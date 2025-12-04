ecd8e54137fd2e568f6ed1466f782d28179a8bbe
Nen Pachani <nenpanchani@gmail.com>
2025-12-04 19:57:10 +0530

    Add initial implementation of class1 with attributes and main method

--- class1.java
+++ class1.java
@@ -1,3 +1,14 @@
 public class class1 {
-    
+    private String colour;
+    private int price;
+
+    public static void main(String[] args) {
+        System.out.println("Driving >>>>>");
+        class1 class1 = new class1();
+        class1.colour = "red";
+        class1.price = 12456;
+
+        System.out.println(class1.colour);
+        System.out.println(class1.price);
+    }
 }
